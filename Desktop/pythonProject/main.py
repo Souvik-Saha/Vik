8312e90ae157cbb5f520a5ca16be996e215b04be
Souvik-Saha <souviksahadgp@gmail.com>
2021-02-15 21:21:46 +0530

    commit v1

--- Desktop/pythonProject/main.py
+++ Desktop/pythonProject/main.py
@@ -12,6 +12,8 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+
     print('PyCharm21_main')
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
